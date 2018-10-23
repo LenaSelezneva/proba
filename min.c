233497ab595be1b02c14807c745967de56a3a763
LenaSelezneva <42895477+LenaSelezneva@users.noreply.github.com>
2018-10-23 13:02:23 +0500

    Update min.c

--- min.c
+++ min.c
@@ -1,9 +1,9 @@
 
-#include"min.h"
 int min_f(int mas[], int lengthMas) {
 	int min;
+	int i;
 	min = mas[0];
-	for (int i = 0; i <= lengthMas; i++) {
+	for (i = 0; i <= lengthMas; i++) {
 		if (mas[i] < min) {
 			min = mas[i];
 		}
